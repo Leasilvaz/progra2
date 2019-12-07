5b79a0b470dded5bec431ac7779ddd2a83ba0216
Leandro <leandrosilvazemn@gmail.com>
2019-12-07 15:50:18 -0300

    grafoTDA e implementacion estatica

---
+++ progra-2/src/Implementaciones/GrafoMA.java
@@ -0,0 +1,56 @@
+package Implementaciones;
+
+import api.ConjuntoTDA;
+import api.GrafoTDA;
+
+public class GrafoMA implements GrafoTDA {
+
+	@Override
+	public void InicializarGrafo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void AgregarVertice(int v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void EliminarVertice(int v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ConjuntoTDA Vertices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void AgegarArista(int v1, int v2, int peso) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void EliminarArista(int v1, int v2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean ExisteArista(int v1, int v2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int PesoArista(int v1, int v2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
